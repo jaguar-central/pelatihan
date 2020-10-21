f7e92b852aa881d742614eebaa4d4216a07e4929
funujikai <fandi771991@gmail.com>
2020-10-21 09:00:19 +0700

    fzl

--- PKU/application/views/pelatihan/modal-ulamm/modaledit.php
+++ PKU/application/views/pelatihan/modal-ulamm/modaledit.php
@@ -66,7 +66,7 @@
 				
 					<label class="col-sm-2">Grade <span class="text-danger">*</span></label>
 					<div class="col-sm-4">
-					<select class="form-control select_tag_edit" required="" id="grading_edit" name="grading_edit">
+					<select class="form-control" required="" id="grading_edit" name="grading_edit">
 							<option value="">--pilih grade--</option>
 
 							<?php 
