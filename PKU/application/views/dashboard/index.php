ddd69799f7281575a2aa76f03c30fdb7d22d66b7
odot <liadirizky@gmail.com>
2020-12-16 16:37:47 +0700

    rzy

--- PKU/application/views/dashboard/index.php
+++ PKU/application/views/dashboard/index.php
@@ -91,117 +91,109 @@
 </div>     -->
 
 
-<?php if (COUNT($top_ten_sub_sektor_mekaar)>0){ ?>
-<div class="welcome">
-      <div class="container-fluid">
-          <div class="row">
-          <div class="col-md-12">
-              <div class="content">
-              <p class="text-center" >Sektor Usaha Mekaar</p> 
-              <hr>
+    <ul class="nav nav-tabs px-4">
+			<li class="nav-item">
+				<a class="nav-link active" data-toggle="tab" href="#kehadiran_mekaar">Sektor Usaha Mekaar</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#kehadiran_ulamm">Sektor Usaha Ulamm</a>
+			</li>
+		</ul>
+
+
+
+<!-- Tab panes -->
+<div class="tab-content px-4">
+				<div id="kehadiran_mekaar" class="tab-pane active">
+						<div class = "table-style" style = "padding : 25px">
+							<div class="form-group row">
+        <?php if (COUNT($top_ten_sub_sektor_mekaar)>0){ ?>
+        <?php $branch='';$x=0; ?>
+            <section class='statis text-center text-white'>
+              <div class="container-fluid">
+                <div class="row">
+                  <?php foreach($top_ten_sub_sektor_mekaar as $data1){ ?>		      
+                  <?php 
+                  if ($branch==''){
+                    $branch=$data1['branch'];
+                  }else if ($data1['branch']==$branch){        
+                    continue;              
+                  }else if ($data1['branch']!=$branch){
+                    $branch=$data1['branch'];                  
+                  }           
+                  
+                  $class = array(
+                  'box bg-primary','box bg-danger','box bg-info','box bg-success','box bg-warning'
+                  );	
+
+                  ?>       
+                  <div class="col-md-4" >
+                    <div class="<?php echo $class[rand(0,4)];$x++; ?>" data-toggle="collapse" data-target="#<?php echo 'sektor_usaha'.$x; ?>">  
+                    <i class="fas fa-address-book"></i>
+                      <h3 class="text-white"><?php echo $data1['DESKRIPSI']; ?></h3>      
+                      <div id="<?php echo 'sektor_usaha'.$x; ?>" class="collapse">
+                      <?php foreach($top_ten_sub_sektor_mekaar as $data2){ ?>
+                        <?php if ($data2['branch']==$data1['branch']){ ?>	            
+                        <p class="lead"><?php echo $data2['SUB_SEKTOR'].' '.$data2['JML']; ?></p>            
+                        <?php } ?>  
+                      <?php } ?>  
+                      </div>
+                    </div>
+                  </div> 
+                  <?php } ?>  
+                </div>
               </div>
-          </div>
-          </div>
-      </div>
-</div>
+            </section>
 
-<?php $branch='';$x=0; ?>
-<section class='statis text-center text-white'>
-  <div class="container-fluid">
-    <div class="row">
-      <?php foreach($top_ten_sub_sektor_mekaar as $data1){ ?>		      
-      <?php 
-      if ($branch==''){
-        $branch=$data1['branch'];
-      }else if ($data1['branch']==$branch){        
-        continue;              
-      }else if ($data1['branch']!=$branch){
-        $branch=$data1['branch'];                  
-      }           
-      
-      $class = array(
-       'box bg-primary','box bg-danger','box bg-info','box bg-success','box bg-warning'
-      );	
+        <?php } ?>
+        </div></div></div>
 
-      ?>       
-      <div class="col-md-4" >
-        <div class="<?php echo $class[rand(0,4)];$x++; ?>" data-toggle="collapse" data-target="#<?php echo 'sektor_usaha'.$x; ?>">  
-        <i class="fas fa-address-book"></i>
-          <h3 class="text-white"><?php echo $data1['DESKRIPSI']; ?></h3>      
-          <div id="<?php echo 'sektor_usaha'.$x; ?>" class="collapse">
-          <?php foreach($top_ten_sub_sektor_mekaar as $data2){ ?>
-            <?php if ($data2['branch']==$data1['branch']){ ?>	            
-            <p class="lead"><?php echo $data2['SUB_SEKTOR'].' '.$data2['JML']; ?></p>            
-            <?php } ?>  
-          <?php } ?>  
+
+<div id="kehadiran_ulamm" class="tab-pane fade">
+	<div class = "table-style" style = "padding : 25px">
+  <div class="form-group row">
+  <?php if (COUNT($top_ten_sub_sektor_ulamm)>0){ ?>
+      <?php $KodeUnit='';$y=0; ?>
+      <section class='statis text-center text-white'>
+        <div class="container-fluid">
+          <div class="row">
+            <?php foreach($top_ten_sub_sektor_ulamm as $data_ulamm1){ ?>		      
+            <?php 
+            if ($KodeUnit==''){
+              $KodeUnit=$data_ulamm1['KodeUnit'];
+            }else if ($data_ulamm1['KodeUnit']==$KodeUnit){        
+              continue;              
+            }else if ($data_ulamm1['KodeUnit']!=$KodeUnit){
+              $KodeUnit=$data_ulamm1['KodeUnit'];                  
+            }           
+            
+            $class = array(
+              'box bg-primary','box bg-danger','box bg-info','box bg-success','box bg-warning'
+            );	
+
+            ?>       
+            <div class="col-md-4" >
+              <div class="<?php echo $class[rand(0,4)];$y++; ?>" data-toggle="collapse" data-target="#<?php echo 'sektor_usaha_ulamm'.$y; ?>">  
+              <i class="fas fa-address-book"></i>
+                <h3 class="text-white"><?php echo $data_ulamm1['DESKRIPSI']; ?></h3>      
+                <div id="<?php echo 'sektor_usaha_ulamm'.$y; ?>" class="collapse">
+                <?php foreach($top_ten_sub_sektor_ulamm as $data_ulamm2){ ?>
+                  <?php if ($data_ulamm1['KodeUnit']==$data_ulamm2['KodeUnit']){ ?>	            
+                  <p class="lead"><?php echo $data_ulamm2['Deskripsi_Bidang_Usaha'].' '.$data_ulamm2['JML']; ?></p>            
+                  <?php } ?>  
+                <?php } ?>  
+                </div>
+              </div>
+              </div> 
+              <?php } ?>  
           </div>
         </div>
-      </div> 
-      <?php } ?>  
-    </div>
+      </section>
+
+    <?php } ?>
   </div>
-</section>
-
-<?php } ?>
-
-
-
-
-
-
-<?php if (COUNT($top_ten_sub_sektor_ulamm)>0){ ?>
-<div class="welcome">
-      <div class="container-fluid">
-          <div class="row">
-          <div class="col-md-12">
-              <div class="content">
-              <p class="text-center" >Sektor Usaha Ulamm</p> 
-              <hr>
-              </div>
-          </div>
-          </div>
-      </div>
 </div>
-
-<?php $KodeUnit='';$y=0; ?>
-<section class='statis text-center text-white'>
-  <div class="container-fluid">
-    <div class="row">
-      <?php foreach($top_ten_sub_sektor_ulamm as $data_ulamm1){ ?>		      
-      <?php 
-      if ($KodeUnit==''){
-        $KodeUnit=$data_ulamm1['KodeUnit'];
-      }else if ($data_ulamm1['KodeUnit']==$KodeUnit){        
-        continue;              
-      }else if ($data_ulamm1['KodeUnit']!=$KodeUnit){
-        $KodeUnit=$data_ulamm1['KodeUnit'];                  
-      }           
-      
-      $class = array(
-        'box bg-primary','box bg-danger','box bg-info','box bg-success','box bg-warning'
-      );	
-
-      ?>       
-      <div class="col-md-4" >
-        <div class="<?php echo $class[rand(0,4)];$y++; ?>" data-toggle="collapse" data-target="#<?php echo 'sektor_usaha_ulamm'.$y; ?>">  
-        <i class="fas fa-address-book"></i>
-          <h3 class="text-white"><?php echo $data_ulamm1['DESKRIPSI']; ?></h3>      
-          <div id="<?php echo 'sektor_usaha_ulamm'.$y; ?>" class="collapse">
-          <?php foreach($top_ten_sub_sektor_ulamm as $data_ulamm2){ ?>
-            <?php if ($data_ulamm1['KodeUnit']==$data_ulamm2['KodeUnit']){ ?>	            
-            <p class="lead"><?php echo $data_ulamm2['Deskripsi_Bidang_Usaha'].' '.$data_ulamm2['JML']; ?></p>            
-            <?php } ?>  
-          <?php } ?>  
-          </div>
-        </div>
-      </div> 
-      <?php } ?>  
-    </div>
-  </div>
-</section>
-
-<?php } ?>
-
+</div>
 </div>
 
 
